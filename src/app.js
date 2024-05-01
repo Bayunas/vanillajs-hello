4a872c05fc5e35d1fa99c8e0dee02387de4d16b5
Daniel Bayona <65182005+Bayunas@users.noreply.github.com>
2024-05-01 17:32:35 +0000

    Excuse generator complete

--- src/app.js
+++ src/app.js
@@ -9,3 +9,37 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird", "The aliens"];
+let action = ["ate", "peed", "crushed", "broke", "attacked"];
+let what = ["my homework", "my phone", "the car", "my house"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying",
+  "while i was taking a shower"
+];
+
+const random = arr => {
+  let randomNum = Math.floor(Math.random() * arr.length);
+  return arr[randomNum];
+};
+
+const myExcuse = () => {
+  document.getElementById("excuse").innerHTML =
+    random(who) +
+    " " +
+    "<b>" +
+    random(action) +
+    "</b>" +
+    " " +
+    "<b>" +
+    random(what) +
+    "</b>" +
+    " " +
+    random(when);
+};
+
+myExcuse();
